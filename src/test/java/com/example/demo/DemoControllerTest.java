ba43be0077679aa5e417caf8233c3e6e6f60dd9a
donchan922 <donchan922@gmail.com>
2019-06-01 11:51:37 +0900

    Controllerのユニットテストを追加

---
+++ src/test/java/com/example/demo/DemoControllerTest.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class DemoControllerTest {
+
+    @Mock
+    private DemoService demoService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new DemoController(demoService)).build();
+    }
+
+    @Test
+    void 正常() throws Exception {
+
+        // 設定
+
+        // 実行
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(status().isOk()).andReturn();
+
+        // 検証
+        assertAll(
+                () -> verify(demoService, times(1)).nextHoliday()
+        );
+    }
+
+    @Test
+    void 正常_例外発生() throws Exception {
+
+        // 設定
+        doThrow(new Exception()).when(demoService).nextHoliday();
+
+        // 実行
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(status().isOk()).andReturn();
+
+        // 検証
+        assertAll(
+                () -> verify(demoService, times(1)).nextHoliday(),
+                () -> assertThrows(Exception.class,
+                        () -> demoService.nextHoliday()
+                )
+        );
+    }
+}
